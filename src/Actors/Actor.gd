d27f747484ddff64012991d4a1b8d3750532f33f
shlee8405 <68775153+shlee8405@users.noreply.github.com>
2020-12-25 20:24:16 +0900

    Add files via upload

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+class_name Actor
+
+
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed: = Vector2(300.0, 3000.0)
+export var gravity: = 2000.0
+var velocity: = Vector2.ZERO
+
+
